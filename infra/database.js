7ed9ab316408c05c6a86bc6c7eda67e1f826f8ad
Joao Marcos de Almeida Rodrigues <joao_rd2015@hotmail.com>
2026-06-19 22:37:52 +0000

    handles database ssl conection

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,13 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("credendiasis do ppostgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
   });
 
   try {
