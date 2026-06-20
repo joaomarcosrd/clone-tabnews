5acdae61568f2e62d921f647f412b1f7005302ef
Joao Marcos de Almeida Rodrigues <joao_rd2015@hotmail.com>
2026-06-20 13:58:17 +0000

    add support for database connection with more flexible and robust SSL configuration

--- infra/database.js
+++ infra/database.js
@@ -6,16 +6,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl:
-      process.env.NODE_ENV === "development"
-        ? false
-        : {
-            rejectUnauthorized: false,
-          },
-  });
-  console.log("credendiasis do ppostgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -33,3 +24,17 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development"
+    ? false
+    : {
+        rejectUnauthorized: false,
+      };
+}
